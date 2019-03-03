2a9582118df8a239a2a3e0f1b57c632afa758a80
Michelle Hoelzer <hoelzermm@gmail.com>
2019-03-03 00:30:39 -0500

    added expectedfailure tests; possibly done correctly

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -5,11 +5,17 @@ from katas import add, multiply, power, factorial, fibonacci
 class TestKatas(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(3, 2), 5)
-        # self.assertEqual(add(3, 3), 5)
+
+    @unittest.expectedFailure
+    def test_add_expected_fail(self):
+        self.assertEqual(add(3, 3), 5)
 
     def test_multiply(self):
         self.assertEqual(multiply(2, 4), 8)
-        # self.assertEqual(multiply(2, 6), 8)
+
+    @unittest.expectedFailure
+    def test_multiply_fail(self):
+        self.assertEqual(multiply(2, 6), 8)
 
     def test_power(self):
         self.assertEqual(power(3, 4), 81)
